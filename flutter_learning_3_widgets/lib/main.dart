bc41094f2ab6f831515cd23c38fcf7cfa63c2208
razvanfulea <razvanfulea@gmail.com>
2021-02-13 23:23:55 +0200

    Module 3 - icons

--- flutter_learning_3_widgets/lib/main.dart
+++ flutter_learning_3_widgets/lib/main.dart
@@ -23,28 +23,70 @@ class MyApp extends StatelessWidget {
                   'https://upload.wikimedia.org/wikipedia/commons/d/d9/Lionel_Messi_20180626_%28cropped%29.jpg',
                 ),
               ),
-              Container(
-                child: Text(
-                  'Lionel Messi',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                    color: Colors.white,
-                  ),
+              Text(
+                'Lionel Messi',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FOOTBALLER',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.white,
+                  letterSpacing: 3.0,
                 ),
               ),
               Container(
-                child: Text(
-                  'FOOTBALLER',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontFamily: 'SourceSansPro',
-                    color: Colors.white,
-                    letterSpacing: 3.0,
-                  ),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '+12 345 678 9900',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0),
+                    )
+                  ],
                 ),
-              )
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'leo@messi.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
