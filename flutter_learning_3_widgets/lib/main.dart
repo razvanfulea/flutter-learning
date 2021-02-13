2dcc5796332650e5e32096f0060184c8e9153607
razvanfulea <razvanfulea@gmail.com>
2021-02-13 23:09:13 +0200

    Module 3 - Custom fonts

--- flutter_learning_3_widgets/lib/main.dart
+++ flutter_learning_3_widgets/lib/main.dart
@@ -6,9 +6,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200.0,
+              ),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/d/d9/Lionel_Messi_20180626_%28cropped%29.jpg',
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Lionel Messi',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'FOOTBALLER',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.white,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
