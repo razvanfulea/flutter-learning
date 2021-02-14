7e569435f87026eb5a2465bfd6fbfeec609cf2dd
razvanfulea <razvanfulea@gmail.com>
2021-02-14 22:52:25 +0200

    Module 5 - refactor code

--- flutter_learning_5_packages/lib/main.dart
+++ flutter_learning_5_packages/lib/main.dart
@@ -4,9 +4,28 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple
+  ];
+
   void playSound(int number) {
     final player = AudioCache();
     player.play('note$number.wav');
+  }
+
+  Widget buildKey({int note, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () => playSound(note),
+        color: color,
+      ),
+    );
   }
 
   @override
@@ -15,36 +34,20 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            children: [
-              FlatButton(
-                onPressed: () => playSound(1),
-                color: Colors.red,
-              ),
-              FlatButton(
-                onPressed: () => playSound(2),
-                color: Colors.orange,
-              ),
-              FlatButton(
-                onPressed: () => playSound(3),
-                color: Colors.yellow,
-              ),
-              FlatButton(
-                onPressed: () => playSound(4),
-                color: Colors.green,
-              ),
-              FlatButton(
-                onPressed: () => playSound(5),
-                color: Colors.blue,
-              ),
-              FlatButton(
-                onPressed: () => playSound(6),
-                color: Colors.indigo,
-              ),
-              FlatButton(
-                onPressed: () => playSound(7),
-                color: Colors.purple,
-              ),
-            ],
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // children: [
+            //   buildKey(note: 1, color: Colors.red),
+            //   buildKey(note: 2, color: Colors.orange),
+            //   buildKey(note: 3, color: Colors.yellow),
+            //   buildKey(note: 4, color: Colors.green),
+            //   buildKey(note: 5, color: Colors.blue),
+            //   buildKey(note: 6, color: Colors.indigo),
+            //   buildKey(note: 7, color: Colors.purple),
+            // ],
+            children: new List.generate(
+              this.colors.length,
+              (index) => buildKey(note: index + 1, color: colors[index]),
+            ).toList(),
           ),
         ),
       ),
