0f8bc39b6fd00131c2c72fed4a4aad57efaab5f6
razvanfulea <razvanfulea@gmail.com>
2021-02-14 16:07:32 +0200

    Module 4 - stateful + randomizer

--- flutter_learning_4_state/lib/main.dart
+++ flutter_learning_4_state/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,15 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 1;
+  int rightDieNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,17 +33,21 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('b1');
+                setState(() {
+                  leftDieNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDieNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('b2');
+                setState(() {
+                  rightDieNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDieNumber.png'),
             ),
           ),
         ],
