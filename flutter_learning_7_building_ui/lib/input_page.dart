f9307f6bc2f2033e1ba52c07d4f48cf993c7fcdd
razvanfulea <razvanfulea@gmail.com>
2021-02-27 23:58:23 +0200

    Module 7 - refactor widgets

--- flutter_learning_7_building_ui/lib/input_page.dart
+++ flutter_learning_7_building_ui/lib/input_page.dart
@@ -9,12 +9,56 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(color: Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+
+  ReusableCard({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: color,
       ),
     );
   }
